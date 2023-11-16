7bf9a11d340b1912c91eeb668def63c3c3beed4d
vitaliy-pg <aljndrvt@myuax.com>
2023-11-16 09:12:59 +0100

    creo un archivo ciencia y dos namaspace de materias distintas

---
+++ ejercicio 4/ciencia.h
@@ -0,0 +1,18 @@
+/ ciencia.h
+#ifndef CIENCIA_H
+#define CIENCIA_H
+
+namespace Ciencia {
+
+    namespace Fisica {
+        extern const double velocidadLuz;  // Constante para la velocidad de la luz
+        double calcularEnergia(double masa);  // Función para calcular la energía
+    }
+
+
+    namespace Matematicas {
+        extern const double pi;
+    }
+}
+
+#endif
