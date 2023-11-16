9af99d53c54221d6867b679e0abe6185cedcf333
vitaliy-pg <aljndrvt@myuax.com>
2023-11-16 08:42:52 +0100

    hago un archivo circulo para calcular su area y añado la constante de pi

---
+++ ejercicio 2/circulo.cpp
@@ -0,0 +1,10 @@
+// circulo.cpp
+#include <iostream>
+
+namespace Geometria {
+    const double PI = 3.14159265358979323846;
+
+    double calcularAreaCirculo(double radio) {
+        return PI * radio * radio;
+    }
+}
