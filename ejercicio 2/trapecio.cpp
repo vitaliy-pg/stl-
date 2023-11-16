b7849eaa8728722fadaced48c93e969d1a59df64
vitaliy-pg <aljndrvt@myuax.com>
2023-11-16 08:46:44 +0100

    hago otro archivo dentro del namespace de geometria para calcular el area del trapecio

---
+++ ejercicio 2/trapecio.cpp
@@ -0,0 +1,8 @@
+// trapecio.cpp
+#include <iostream>
+
+namespace Geometria {
+    double calcularAreaTrapecio(double baseMayor, double baseMenor, double altura) {
+        return 0.5 * (baseMayor + baseMenor) * altura;
+    }
+}
