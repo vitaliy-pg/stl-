21cacde9775ba4d030e39abf1680e4a39633f797
vitaliy-pg <aljndrvt@myuax.com>
2023-11-16 09:27:19 +0100

    declaro la funcion principal de vector de numeros

--- ejercicio 5/STL.cpp
+++ ejercicio 5/STL.cpp
@@ -2,3 +2,17 @@
 #include <vector>
 #include <map>
 #include <set>
+int main() {
+
+}
+    std::vector<int> numeros = {1, 2, 3, 4, 5};
+
+
+    numeros.push_back(6);
+
+
+    std::cout << "Vector de números: ";
+    for (const auto& numero : numeros) {
+        std::cout << numero << " ";
+    }
+    std::cout << std::endl;
